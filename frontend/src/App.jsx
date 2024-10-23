71d1d8f07ac588a87fcc8225697855f8a2c6e1c2
SUNDU TARUN <114809192+Tarunsundu111@users.noreply.github.com>
2024-10-23 16:42:26 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Router, Routes } from 'react-router-dom'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
